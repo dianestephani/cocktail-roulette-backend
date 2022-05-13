606c997b592016dd3448b17fb390dc91a02668e6
Diane Stephani <diane.stephani@gmail.com>
2022-05-12 19:09:22 -0700

    prints specified index item from spirits array

--- App.js
+++ App.js
@@ -4,8 +4,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+let spiritName = require( './routes/inventory');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(`Hello World! The spirit chosen is ${spiritName}` )
 })
 
 app.listen(port, () => {
